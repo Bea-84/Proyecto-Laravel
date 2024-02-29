6f11e98925564d9be72bb2dba2d7746d3aebdc4d
Bea-84 <bea_erlin93@alumnes.ilernabcn.com>
2024-02-29 13:15:34 +0100

    Primer comit

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(){
+        return "Hola mundo";
+    }
+}
