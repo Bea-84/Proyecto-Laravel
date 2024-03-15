7b662b0a509436eb8646f6f1c75d31a746995ea8
Bea-84 <bea_erlin93@alumnes.ilernabcn.com>
2024-03-15 09:04:05 +0100

    Editar asistencia OK

--- app/Http/Controllers/Api/AsistenciaController.php
+++ app/Http/Controllers/Api/AsistenciaController.php
@@ -8,34 +8,34 @@ use App\Models\Asistencia;
 
 class AsistenciaController extends Controller
 {
-      //Función que nos devuelve todos los datos de asistencia que hay en la BBDD por get request ponemos url(para comprobar)
+      //Función que nos devuelve todos los datos de asistencia que hay en la BBDD 
       public function index(){
         $asistencias = Asistencia::all(); 
         return $asistencias;
-     }
+      }
 
-     //Función que crea un nuevo dato de asistencia en la BBDD por post request ponemos url(para comprobar)
-    public function store( Request $request){
+     //Función que crea un nuevo dato de asistencia en la BBDD 
+     public function store( Request $request){
 
-      //Validar datos que tienen que estar obligatorios
-      $request->validate([
-          'asistencia'=>'required',
-          'fecha'=>'required',
-      ]);
+        //Validar datos que tienen que estar obligatorios
+        $request->validate([
+            'asistencia'=>'required',
+            'fecha'=>'required',
+        ]);
 
-      $data=$request->all();
+        $data=$request->all();
 
-      $asistencias = Asistencia::create($data);
-      $response=[
-          'success'=>true,
-          'message'=>'Asistencia creada',
-          'data'=>$asistencias
-      ];
+        $asistencias = Asistencia::create($data);
+        $response=[
+            'success'=>true,
+            'message'=>'Asistencia creada',
+            'data'=>$asistencias
+        ];
 
-      return response()->json($response);
-  }
+        return response()->json($response);
+      }
 
-    //Función editar datos de asistencia por put request ponemos url/ id tarea
+    //Función editar datos de asistencia en la BBDD
     public function update(Request $request, $id){
 
       //Validar datos que tienen que estar obligatorios
@@ -44,19 +44,44 @@ class AsistenciaController extends Controller
         'fecha'=>'required',
         'user_id'=>'required',
         
-    ]);
+      ]);
 
-   $data=$request->all();
+      $data=$request->all();
 
-   $asistencia = Asistencia::find($id);
-   $asistencia->update($data);
+      $asistencia = Asistencia::find($id);
+      $asistencia->update($data);
    
-   $response=[
-       'success'=>true,
-       'message'=>'Asistencia actualizada',
-       'data'=>$asistencia
-   ];
+      $response=[
+          'success'=>true,
+          'message'=>'Asistencia actualizada',
+          'data'=>$asistencia
+      ];
 
-   return response()->json($response);
-}
+      return response()->json($response);
+    }
+
+    //Función para buscar dato por su id
+    public function show($id) {
+      // Buscar la asistencia por su ID
+      $asistencia = Asistencia::find($id);
+  
+      // Verificar si se encontró la asistencia
+      if (!$asistencia) {
+          $response = [
+              'success' => false,
+              'message' => 'Asistencia no encontrada',
+              'data' => null
+          ];
+          return response()->json($response, 404);
+      }
+  
+      // Si se encontró la asistencia, retornarla en la respuesta JSON
+      $response = [
+          'success' => true,
+          'message' => 'Asistencia encontrada',
+          'data' => $asistencia
+      ];
+      return response()->json($response);
+  }
+  
 }
