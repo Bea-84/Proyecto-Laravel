7d4914927e1678093cba8b62cfb52bdaec48f91a
Ramon Pamias <ramonpamias@gmail.com>
2024-04-20 11:05:40 +0200

    Acabado aviso legal y añadido fondo negro al footer

--- resources/js/layouts/AppFooter.vue
+++ resources/js/layouts/AppFooter.vue
@@ -24,4 +24,8 @@ const logoUrl = computed(() => {
         </ul>
     </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .layout-footer {
+        background-color: black;
+    }
+</style>
--- resources/js/views/avisolegal/index.vue
+++ resources/js/views/avisolegal/index.vue
@@ -35,12 +35,103 @@
         <p>Una dirección IP es un número asignado automáticamente a un ordenador cuando éste se conecta a Internet. Toda esta información es registrada en un fichero de actividad
         del servidor, debidamente inscrito, que permite el posterior procesamiento de los datos con el fin de obtener mediciones únicamente estadísticas que permitan conocer
         el número de impresiones de páginas, el número de visitas realizadas a los servidores web, el orden de visitas, el punto de acceso, etc.</p>
+        <br>
+        <br>
+        <h5>POLITICA DE ENLACES</h5>
+        <p>Desde el presente sitio web es posible que se redirija a contenidos de terceros sitios web. Dado que El Responsable no puede controlar siempre los contenidos introducidos
+        por los terceros en sus sitios web, éste no asume ningún tipo de responsabilidad respecto a dichos contenidos. En todo caso, El Responsable manifiesta que procederá a la
+        retirada inmediata de cualquier contenido que pudiera contravenir la legislación nacional o internacional, la moral o el orden público, procediendo a la retirada
+        inmediata de la redirección a dicho sitio web, poniendo en conocimiento de las autoridades competentes el contenido en cuestión.</p>
+        <p>El Responsable no se hace responsable de la información y contenidos almacenados, a título enunciativo pero no limitativo, en foros, chat´s, generadores de blogs, 
+        comentarios, redes sociales o cualquier otro medio que permita a terceros publicar contenidos de forma independiente en la página web del Responsable. No obstante y en 
+        cumplimiento de lo dispuesto en el art. 11 y 16 de la LSSICE, El Responsable se pone a disposición de todos los usuarios, autoridades y fuerzas de seguridad, colaborando
+        de forma activa en la retirada o en su caso bloqueo de todos aquellos contenidos que pudieran afectar o contravenir la legislación nacional, o internacional, derechos de
+        terceros o la moral y el orden público. En caso de que el usuario considere que existe en el sitio web algún contenido que pudiera ser susceptible de esta clasificación, 
+        se ruega lo notifique de forma inmediata al administrador del sitio web.</p>
+        <p>Este sitio web ha sido revisado y probado para que funcione correctamente. En principio, puede garantizarse el correcto funcionamiento los 365 días del año, 24 horas al día. 
+        No obstante, El Responsable no descarta la posibilidad de que existan ciertos errores de programación, o que acontezcan causas de fuerza mayor, catástrofes naturales, huelgas,
+        o circunstancias semejantes que hagan imposible el acceso a la página web.</p>
+        <br>
+        <br>
+        <h5>PROTECCIÓN DE DATOS</h5>
+        <p>El Responsable se encuentra profundamente comprometido en el cumplimiento de lo dispuesto por la normativa vigente en materia de protección de datos, y en el contexto
+        del nuevo marco normativo establecido en virtud de lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la
+        protección de las personas físicas en lo que respecta al tratamiento de datos personales y a la libre circulación de estos datos (en adelante, el “RGPD”), y la normativa nacional
+        en materia de protección de datos de carácter personal.
+        </p>
+        <p>Asimismo, El Responsable informa que da cumplimiento a la Ley 34/2002 de 11 de julio, de Servicios de la Sociedad de la Información y el Comercio Electrónico y le solicitará
+        su consentimiento al tratamiento de sus datos con fines comerciales en cada momento.</p>
+        <br>
+        <br>
+        <h5>PROPIEDAD INTELECTUAL E INDUSTRIAL</h5>
+        <p>El sitio web, incluyendo a título enunciativo pero no limitativo su programación, edición, compilación y demás elementos necesarios para su funcionamiento, los diseños, logotipos,
+        texto y/o gráficos son propiedad del Responsable o en su caso dispone de licencia o autorización expresa por parte de los autores.
+        </p>
+        <p>Todos los contenidos del sitio web se encuentran debidamente protegidos por la normativa de propiedad intelectual e industrial.</p>
+        <p>Independientemente de la finalidad para la que fueran destinados, la reproducción total o parcial, uso, explotación, distribución y comercialización, requiere en todo caso de la
+        autorización escrita previa por parte del Responsable. Cualquier uso no autorizado previamente por parte del Responsable será considerado un incumplimiento grave de los derechos
+        de propiedad intelectual o industrial del autor.</p>
+        <p>Los diseños, logotipos, texto y/o gráficos ajenos al prestador y que pudieran aparecer en el sitio web, pertenecen a sus respectivos propietarios, siendo ellos mismos responsables
+        de cualquier posible controversia que pudiera suscitarse respecto a los mismos. En todo caso, El Responsable cuenta con la autorización expresa y previa por parte de los mismos.</p>
+        <p>El Responsable autoriza expresamente a que terceros puedan redirigir directamente a los contenidos concretos del sitio web, debiendo en todo caso redirigir a la página 
+        principal del sitio web del Responsable.</p>
+        <p>El Responsable reconoce a favor de sus titulares los correspondientes derechos de propiedad intelectual e industrial, no implicando su sola mención o aparición en el sitio web la
+        existencia de derechos o responsabilidad alguna del Responsable sobre los mismos, como tampoco respaldo, patrocinio o recomendación por su parte.</p>
+        <p>Para todas aquellas imágenes en las que el software de diseño lo permite, se han incluido las referencias correspondientes a la autoría y licencia correspondiente.</p>
+        <p>Para realizar cualquier tipo de observación respecto a posibles incumplimientos de los derechos de propiedad intelectual o industrial, así como sobre cualquiera de los 
+        contenidos del sitio web, puede hacerlo a través del siguiente correo electrónico: info@zonadecombate.es</p>
+        <br>
+        <br>
+        <h5>POLÍTICA DE PRIVACIDAD</h5>
+        <p><strong>1.- Información al Usuario</strong></p>
+        <p><strong>Jose Maria Guerrero Torvisco</strong> informa a los usuarios del sitio web sobre su política respecto del tratamiento y protección de los datos de carácter personal de los usuarios y
+        clientes que puedan ser recabados por la navegación a través de su sitio web.</p>
+        <p><strong>Jose Maria Guerrero Torvisco</strong> es el Responsable del tratamiento de los datos personales del Usuario, por lo que se le facilita la siguiente información referente al tratamiento 
+        de sus datos:</p>
+        <p>Criterios de conservación de los datos: Los datos personales proporcionados se conservarán hasta que no se solicite su suspensión por parte del interesado, ejerciendo sus 
+          derechos sobre los datos, y en cualquier caso en cumplimiento de plazos legales de prescripción que le resulten de aplicación.</p>
+        <p><strong>Derechos de los interesados:</strong></p>
+        <ul>
+            <li>Derecho a retirar el consentimiento en cualquier momento.</li>
+            <li>Derecho de acceso, rectificación, supresión, portabilidad de sus datos y a la limitación u oposición de su tratamiento, así como no ser objeto de decisiones individuales automatizadas.</li>
+        </ul>
+        <p>Datos de contacto para ejercer sus derechos:</p>
+        <p>Debe identificarse debidamente y solicitar claramente el derecho a ejercer.</p>
+        <ul>
+            <li><strong>Dirección postal: C/industria 35 1reo C. Entrada por calle Milá 6, 08291 Ripollet</strong></li>
+            <li><strong>Dirección electrónica: info@zonadecombate.es</strong></li>
+        </ul>
+        <br>
+        <p><strong>2.- Información facilitada por el usuario</strong></p>
+        <p>Si eres menor de 18 años no está permitido el uso de formularios en nuestra web.</p>
+        <p>Los Usuarios, introduciendo sus datos en los formularios de contacto o presentados en formularios de descarga, aceptan expresamente y de forma libre e inequívoca, que sus datos son necesarios para 
+        atender su petición, por parte del Responsable, siendo voluntaria la inclusión de datos en los campos restantes.</p>
+        <p>El Usuario garantiza que los datos personales facilitados son veraces y se hace responsable de comunicar cualquier modificación de los mismos.</p>
+        <p>Todos los datos solicitados a través del sitio web son necesarios para la prestación de un servicio óptimo al Usuario. En caso de que no sean facilitados todos los datos, no se garantiza que la información
+         y servicios que El Responsable le facilite, sean completamente ajustados a sus necesidades.</p>
+        <br>
+        <p><strong>3.- Medidas de seguridad</strong></p>
+        <p>Que de conformidad con lo dispuesto en las normativas vigentes en materia de protección de datos personales, El Responsable está cumpliendo con todas las disposiciones de las normativas RGPD y LOPD para
+        el tratamiento de los datos personales de su responsabilidad, los cuales son tratados de manera lícita, leal y transparente en relación con el interesado y adecuados, pertinentes y limitados a lo necesario
+        en relación con los fines para los que son tratados.</p>
+        <p>El Responsable garantiza que ha implementado políticas técnicas y organizativas apropiadas para aplicar las medidas de seguridad que establecen el RGPD y la LOPD con el fin de proteger los derechos y libertades
+        de los Usuarios y les ha comunicado la información adecuada para que puedan ejercerlos.</p>
+        <br>
+        <p><strong>Brechas de seguridad</strong></p>
+        <p>El Responsable informará sobre cualquier brecha de seguridad que afecte a la base de datos que utiliza este sitio web, o que afecte a cualquiera de nuestros servicios de terceros, a todas y cada una de 
+        las personas, datos de los cuales puedan haber sido afectadas, y autoridades, dentro de las 72 horas siguientes a la detección de la brecha.</p>
+        <br>
+        <br>
+        <h5>LEGISLACIÓN APLICABLE Y JURISDICCIÓN</h5>
+        <p>Para la resolución de todas las controversias o cuestiones relacionadas con el presente sitio web o de las actividades en él desarrolladas, será de aplicación la legislación española, a la que se someten
+        expresamente las partes, siendo competentes para la resolución de todos los conflictos derivados o relacionados con su uso los Juzgados y Tribunales de Barcelona.</p>
     </div>
+    <AppFooter />
 </template>
   
-  <script setup>
-  
-  </script>
+<script setup>
+import AppFooter from '../../layouts/AppFooter.vue';
+</script>
   
   <style scoped>
     h5 {
@@ -49,6 +140,13 @@
     }
 
     p {
+        margin-left: 10%;
+        margin-right: 10%;
+        justify-content: center;
+        color: whitesmoke;
+    }
+
+    li {
         margin-left: 10%;
         margin-right: 10%;
         justify-content: center;
