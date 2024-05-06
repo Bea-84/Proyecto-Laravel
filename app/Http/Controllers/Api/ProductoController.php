871dddf4f7a317009afe47703ef02394486a513e
Ramón Pàmias <ramonpamias@gmail.com>
2024-05-06 12:57:08 +0200

    Añadidas funciones para editar y modificar en ProductoController.php

--- app/Http/Controllers/Api/ProductoController.php
+++ app/Http/Controllers/Api/ProductoController.php
@@ -34,4 +34,63 @@ class ProductoController extends Controller {
 
       return response()->json($response);
   }
+
+  //Función editar datos de prodcutos que se guardan en la BBDD
+  public function update(Request $request, $id){
+
+    //Validar datos que tienen que estar obligatorios
+    $request->validate([
+        'descripcion'=>'required',
+        'precio'=>'required',
+        'imagen'=>'required'
+      
+      
+    ]);
+
+    $data=$request->all();
+
+    $producto = Producto::find($id);
+    $producto->update($data);
+ 
+    $response=[
+        'success'=>true,
+        'message'=>'Producto actualizado',
+        'data'=>$producto
+    ];
+
+    return response()->json($response);
+  }
+
+   //Función para buscar dato por su id y mostrar
+   public function show($id) {
+    // Buscar la asistencia por su ID
+    $producto = Producto::find($id);
+
+    // Verificar si se encontró la asistencia
+    if (!$producto) {
+        $response = [
+            'success' => false,
+            'message' => 'Producto no encontrado',
+            'data' => null
+        ];
+        return response()->json($response, 404);
+    }
+
+    // Si se encontró el producto, retornarlo en la respuesta JSON
+    $response = [
+        'success' => true,
+        'message' => 'Producto encontrado',
+        'data' => $producto
+    ];
+    return response()->json($response);
+}
+
+//Función para eliminar un producto de la BBDD
+public function destroy($id){
+
+    $producto = Producto::find($id);
+    $producto->delete();
+    return response()->json(['success' => true, 'message' => 'Producto eliminado correctamente']);
+}
+
 }
